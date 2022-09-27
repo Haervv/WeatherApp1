f95957124dcb4ab95e7ed409b8cb975d9c4763f7
Haervv <viktorov_vitaliy@mail.ru>
2022-09-27 21:20:56 +0300

    13less

--- app/src/main/java/com/VitaliyProj/weatherapp/Fragments/MainFragment.kt
+++ app/src/main/java/com/VitaliyProj/weatherapp/Fragments/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.VitaliyProj.weatherapp.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,10 +14,15 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.registerForActivityResult
 import androidx.fragment.app.FragmentActivity
 import com.VitaliyProj.weatherapp.adapters.VpAdapter
+import com.VitaliyProj.weatherapp.adapters.WeatherModel
 import com.VitaliyProj.weatherapp.databinding.FragmentMainBinding
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.tabs.TabLayoutMediator
+import org.json.JSONObject
 import java.util.jar.Manifest
-
+const val API_KEY = "f0170757eeb24053bf4180424222203"
 class MainFragment : Fragment() {
     private val fList = listOf(
         HoursFragment.newInstance(),
@@ -41,6 +47,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        requestWeatherData("London")
     }
 
     private fun init() = with(binding){
@@ -64,6 +71,74 @@ class MainFragment : Fragment() {
             permissionListener()
             pLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city: String){
+        val url =
+            "https://api.weatherapi.com/v1/forecast.json?key=" +
+                    API_KEY +
+                    "&q=" +
+                    city +
+                    "&days=" +
+                    "2" +
+                    "&aqi=no&alerts=no\n"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result -> parseWeatherData(result)
+
+            },
+            {
+                error -> Log.d("MyLog","error: $error")
+            }
+        )
+        queue.add(request)
+    }
+
+    private fun parseWeatherData(result: String){
+        val mainObject = JSONObject(result)
+        val list = parseDays(mainObject)
+        parseCurrentData(mainObject, list[0])
+    }
+
+    private fun parseDays(mainObject: JSONObject): List<WeatherModel>{
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast")
+            .getJSONArray("forecastday")
+        val name = mainObject.getJSONObject("location").getString("name")
+        for (i in 0 until daysArray.length()){
+            val day = daysArray[i] as JSONObject
+            val item = WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition").getString("text"),
+                "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day").getJSONObject("condition").getString("icon"),
+                day.getJSONArray("hour").toString()
+            )
+            list.add(item)
+        }
+        return list
+    }
+
+    private fun parseCurrentData(mainObject: JSONObject, weatherItem: WeatherModel){
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            weatherItem.maxTemp,
+            weatherItem.minTemp,
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("icon"),
+            weatherItem.hours
+        )
+        Log.d("MyLog","City: ${item.maxTemp}")
+        Log.d("MyLog","Time: ${item.minTemp}")
+        Log.d("MyLog","Time: ${item.hours}")
     }
 
     companion object {
